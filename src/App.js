6cadf4b2adf2e1942af80bdae5a395e1d6044f20
JefAnubis <yeffricelulares@gmail.com>
2021-10-12 12:27:40 -0500

    se agrego carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
-
+import Carousel from './Components/Carousel';
 function App() {
   return (
       <div className="App">
-          <NavBar/>
+          
+          <Carousel/>
     </div>
   );
 }
